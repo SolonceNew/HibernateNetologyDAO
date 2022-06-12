bc4ca263b6eaa4b76620383a432afda30ed15f4c
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2022-06-12 22:50:36 +0300

    jpa query

--- src/main/java/ru/netology/hibernate/controller/Controller.java
+++ src/main/java/ru/netology/hibernate/controller/Controller.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.hibernate.entity.Person;
@@ -14,24 +15,24 @@ import java.util.Optional;
 
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@RequestMapping("/persons")
 @RestController
 public class Controller {
     Service service;
 
-    @GetMapping("/persons/by-city")
-    public List<Person> getPersonByCity(@RequestParam("city") String city) {
+    @GetMapping("/by-city")
+    public List<Person> getPersonByCity(String city) {
         return service.getPersonByCity(city);
     }
 
 
-    @GetMapping("/persons/by-age")
-    public List<Person> findAllByPersonId_AgeLessThanOrderByPersonId(@RequestParam("age") int age) {
+    @GetMapping("/by-age")
+    public List<Person> findAllByPersonId_AgeLessThanOrderByPersonId(int age) {
         return service.findAllByPersonId_AgeLessThanOrderByPersonId(age);
     }
 
-    @GetMapping("/persons/by-name")
-    public Optional<Person> findPersonByPersonId_NameAndPersonId_Surname(@RequestParam("name") String name,
-                                                                  @RequestParam("surname") String surname) {
+    @GetMapping("by-name-and-surname")
+    public Optional<Person> findPersonByPersonId_NameAndPersonId_Surname(String name, String surname) {
         return service.findPersonByPersonId_NameAndPersonId_Surname(name, surname);
     }
 }
--- src/main/java/ru/netology/hibernate/repository/Repository.java
+++ src/main/java/ru/netology/hibernate/repository/Repository.java
@@ -3,6 +3,8 @@ package ru.netology.hibernate.repository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.netology.hibernate.entity.Person;
 import ru.netology.hibernate.entity.PersonId;
 
@@ -15,10 +17,13 @@ import java.util.Optional;
 @org.springframework.stereotype.Repository
 public interface Repository extends JpaRepository<Person, PersonId> {
 
-    List<Person> findPersonByCityOfLiving(String city);
+    @Query("select p from Person p where p.cityOfLiving = :cityOfLiving")
+    List<Person> findPersonByCityOfLiving(@Param("city") String city);
 
-    List<Person> findAllByPersonId_AgeLessThanOrderByPersonId_Age(int age);
+    @Query("select p from Person p where p.personId.age < :age order by p.personId.age asc")
+    List<Person> findAllByPersonId_AgeLessThanOrderByPersonId_Age(@Param("age") int age);
 
-
-    Optional<Person> findPersonByPersonId_NameAndPersonId_Surname(String name, String surname);
+    @Query("select p from Person p where p.personId.name = :name and p.personId.surname = :surname")
+    Optional<Person> findPersonByPersonId_NameAndPersonId_Surname(@Param("name") String name,
+                                                                  @Param("surname") String surname);
 }
